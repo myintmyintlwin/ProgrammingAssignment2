11f47044f792ebcea4449171d03f6bdaa651e69e
Myint Myint Lwin <myintmyintlwin@uit.edu.mm>
2020-08-24 23:28:44 +0630

    MMl's code v1

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+  inv_matrix<- NULL
+  set <- function(y) {
+    x <<- y
+    inv_matrix<<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv_matrix <<- inverse
+  getInverse <- function() inv_matrix
+  list(set = set,
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
 
 
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return inverted matrix of x
+  
+  inv_matrix <- x$getInverse()
+  if (!is.null(inv_matrix)) {
+    message("getting cached matrix")
+    return(inv_matrix)
+  }
+  mat <- x$get()
+  inv_matrix <- solve(mat, ...)
+  x$setInverse(inv_matrix)
+  inv_matrix
+  
 }
